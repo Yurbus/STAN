dc52655f2300a03fa8f0600ab237d750e71f84f6
Yurbus <frankins636@gmail.com>
2025-08-22 18:54:34 +0300

    gfggftt

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,78 +27,6 @@ const isMobile = {
 };
 
 
-// Прокрутка при клике
-// const menuLinks = document.querySelectorAll('.menu__link a[data-goto]');
-// if(menuLinks.length > 0) {
-// 	menuLinks.forEach(menuLink => {
-// 		menuLink.addEventListener("click", onMenuLinkClick);
-// 	});
-
-// 	function onMenuLinkClick(e) {
-// 		const menuLink = e.target;
-// 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
-// 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
-// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
-		
-// 			if (iconMenu.classList.contains('_active')) {
-// 				document.body.classList.remove('_lock');
-// 				iconMenu.classList.remove('_active');
-// 				menuBody.classList.remove('_active');
-// 			}
-
-// 			window.scrollTo({
-// 				top: gotoBlockValue,
-// 				behavior: "smooth"
-// 			});
-// 			e.preventDefault();
-// 		}
-// 	}
-// }
-
-// Active link
-// document.addEventListener('DOMContentLoaded', () => {
-//     const menuLinks = document.querySelectorAll('.menu__list a[data-goto]');
-//     const sections = [];
-
-//     menuLinks.forEach(link => {
-//         const selector = link.dataset.goto;
-//         const section = document.querySelector(selector);
-//         if (section) {
-//             sections.push({
-//                 link: link,
-//                 section: section
-//             });
-//         }
-//     });
-
-//     function onScroll() {
-//         let scrollY = window.scrollY;
-
-//         sections.forEach(({ link, section }) => {
-//             const sectionTop = section.offsetTop - 100;
-//             const sectionHeight = section.offsetHeight;
-//             const isActive = scrollY >= sectionTop && scrollY < sectionTop + sectionHeight;
-
-//             link.classList.toggle('active', isActive);
-//         });
-//     }
-
-//     window.addEventListener('scroll', onScroll);
-//     onScroll(); // вызов при загрузке
-// });
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     const currentPage = location.pathname.split('/').pop(); // получаем имя файла
-
-//     document.querySelectorAll('.menu__list a').forEach(link => {
-//         const href = link.getAttribute('href');
-//         if (href === currentPage) {
-//             link.classList.add('active');
-//         }
-//     });
-// });
-
-
 // Меню бурнер
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.header__right');
@@ -236,6 +164,83 @@ if (isMobile.any()) {
 } else {
     document.body.classList.add('_pc');
 }
+
+
+
+
+
+// Прокрутка при клике
+// const menuLinks = document.querySelectorAll('.menu__link a[data-goto]');
+// if(menuLinks.length > 0) {
+// 	menuLinks.forEach(menuLink => {
+// 		menuLink.addEventListener("click", onMenuLinkClick);
+// 	});
+
+// 	function onMenuLinkClick(e) {
+// 		const menuLink = e.target;
+// 		if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+// 			const gotoBlock = document.querySelector(menuLink.dataset.goto);
+// 			const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+		
+// 			if (iconMenu.classList.contains('_active')) {
+// 				document.body.classList.remove('_lock');
+// 				iconMenu.classList.remove('_active');
+// 				menuBody.classList.remove('_active');
+// 			}
+
+// 			window.scrollTo({
+// 				top: gotoBlockValue,
+// 				behavior: "smooth"
+// 			});
+// 			e.preventDefault();
+// 		}
+// 	}
+// }
+
+// Active link
+// document.addEventListener('DOMContentLoaded', () => {
+//     const menuLinks = document.querySelectorAll('.menu__list a[data-goto]');
+//     const sections = [];
+
+//     menuLinks.forEach(link => {
+//         const selector = link.dataset.goto;
+//         const section = document.querySelector(selector);
+//         if (section) {
+//             sections.push({
+//                 link: link,
+//                 section: section
+//             });
+//         }
+//     });
+
+//     function onScroll() {
+//         let scrollY = window.scrollY;
+
+//         sections.forEach(({ link, section }) => {
+//             const sectionTop = section.offsetTop - 100;
+//             const sectionHeight = section.offsetHeight;
+//             const isActive = scrollY >= sectionTop && scrollY < sectionTop + sectionHeight;
+
+//             link.classList.toggle('active', isActive);
+//         });
+//     }
+
+//     window.addEventListener('scroll', onScroll);
+//     onScroll(); // вызов при загрузке
+// });
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const currentPage = location.pathname.split('/').pop(); // получаем имя файла
+
+//     document.querySelectorAll('.menu__list a').forEach(link => {
+//         const href = link.getAttribute('href');
+//         if (href === currentPage) {
+//             link.classList.add('active');
+//         }
+//     });
+// });
+
+
 
 
 // -------- Accordion -----------------------------------------
